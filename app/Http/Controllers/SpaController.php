958a4d521d794fd2590666f8b7d7da1ca8341dc9
Eugene Zavoloka <eugenezm@gmail.com>
2021-10-31 19:27:14 +0200

    Initial commit

---
+++ app/Http/Controllers/SpaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\SpaController;
+use Illuminate\Http\Request;
+
+class SpaController extends Controller
+{
+    public function index() 
+    
+
+    return view('index');
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+
+const app = new Vue({
+	el: '#app',
+})
